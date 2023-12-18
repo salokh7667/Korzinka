133ed1bedaa64ccaa02b864f122a3a0b4520d786
salokhiddin <143103925+salokh7667@users.noreply.github.com>
2023-12-18 14:45:08 +0300

    sdfghjk
    dsfghjkl

---
+++ style.js
@@ -0,0 +1,26 @@
+let a = prompt("1-soningizni kiriting")
+let b = prompt("2-soni kiriting")
+let c = prompt("3-soni kiriting")
+
+if(a == b == c){
+    console.log("no");
+}else if(a != b != c){
+    console.log("yes");
+}
+
+// if(b > c){
+//     console.log(b  + " b katta son");
+// }
+// if(a > b){
+//     console.log(a  + " a katta son");
+// }else if(a == b == c) {
+//     console.log("hamma son teng " + a + " = " + b + "=" + c + "ga" );
+// }else if(a == c) {
+//     console.log("ikkala son ham teng " + a + " = " + c + "ga" );
+// }else if(b == c) {
+//     console.log("ikkala son ham teng " + b + " = " + c + "ga" );
+// }else if(a == b) {
+//     console.log("ikkala son ham teng " + a + " = " + b + "ga" );
+// }else{
+//     console.log(c + " c katta son");   
+// }
